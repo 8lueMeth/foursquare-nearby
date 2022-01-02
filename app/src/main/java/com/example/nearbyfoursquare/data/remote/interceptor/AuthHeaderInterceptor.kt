958f6ebd7da87133381d808ee4761863e9ca171a
Mohsen Farahani <methdfarahani@gmail.com>
2022-01-02 18:57:23 +0330

    Implement AuthHeaderInterceptor

---
+++ app/src/main/java/com/example/nearbyfoursquare/data/remote/interceptor/AuthHeaderInterceptor.kt
@@ -0,0 +1,16 @@
+package com.example.nearbyfoursquare.data.remote.interceptor
+
+import com.example.nearbyfoursquare.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class AuthHeaderInterceptor @Inject constructor() : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val original = chain.request()
+        val request = original.newBuilder()
+        request.addHeader("Authorization", "Bearer ${BuildConfig.ACCESS_TOKEN}")
+        return chain.proceed(request.build())
+    }
+}
