554d0682d262810226b0a9bf05debbfc13301361
Mohsen Farahani <methdfarahani@gmail.com>
2022-01-08 15:41:31 +0330

    Setup MainActiviy And Its NavGraph

--- app/src/main/java/com/example/nearbyfoursquare/MainActivity.kt
+++ app/src/main/java/com/example/nearbyfoursquare/MainActivity.kt
@@ -3,36 +3,62 @@ package com.example.nearbyfoursquare
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.paging.ExperimentalPagingApi
+import com.example.nearbyfoursquare.ui.Screens
+import com.example.nearbyfoursquare.ui.places.list.PlacesScreen
+import com.example.nearbyfoursquare.ui.places.list.PlacesViewModel
 import com.example.nearbyfoursquare.ui.theme.NearbyFoursquareTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.ObsoleteCoroutinesApi
 
+@ObsoleteCoroutinesApi
+@ExperimentalCoroutinesApi
+@ExperimentalPermissionsApi
+@ExperimentalPagingApi
+@ExperimentalMaterialApi
+@ExperimentalComposeUiApi
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NearbyFoursquareTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Screens.Places.route,
+                    builder = {
+                        addPlacesScreen(navController = navController)
+                    }
+                )
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NearbyFoursquareTheme {
-        Greeting("Android")
+@ObsoleteCoroutinesApi
+@ExperimentalPermissionsApi
+@ExperimentalCoroutinesApi
+@ExperimentalPagingApi
+@ExperimentalComposeUiApi
+@ExperimentalMaterialApi
+fun NavGraphBuilder.addPlacesScreen(
+    navController: NavHostController,
+) {
+    composable(
+        route = Screens.Places.route,
+    ) {
+        val viewModel: PlacesViewModel = hiltViewModel()
+        PlacesScreen(viewModel = viewModel)
     }
 }
