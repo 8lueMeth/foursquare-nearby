c2dc4ae9aff8f4d2dd20287228ad661c7c5dcdc1
Mohsen Farahani <methdfarahani@gmail.com>
2022-01-09 14:27:56 +0330

    Add PlaceDetailsDao To PlacesDatabase

--- app/src/main/java/com/example/nearbyfoursquare/data/local/PlacesDatabase.kt
+++ app/src/main/java/com/example/nearbyfoursquare/data/local/PlacesDatabase.kt
@@ -2,14 +2,17 @@ package com.example.nearbyfoursquare.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.nearbyfoursquare.data.local.dao.PlaceDetailsDao
 import com.example.nearbyfoursquare.data.local.dao.PlacesDao
-import com.example.nearbyfoursquare.data.local.entity.PlacesEntity
+import com.example.nearbyfoursquare.data.local.entity.PlaceDetailsEntity
+import com.example.nearbyfoursquare.data.local.entity.PlaceEntity
 
 @Database(
-    entities = [PlacesEntity::class],
+    entities = [PlaceEntity::class, PlaceDetailsEntity::class],
     version = 1,
     exportSchema = false
 )
 abstract class PlacesDatabase : RoomDatabase() {
     abstract fun placesDao(): PlacesDao
+    abstract fun placeDetailsDao(): PlaceDetailsDao
 }
