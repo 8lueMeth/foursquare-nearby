22f7e6c5699db161f389f87a36ce5cd5d091bbed
Mohsen Farahani <methdfarahani@gmail.com>
2022-01-09 14:27:18 +0330

    Refactor Entity Name In PlacesDao

--- app/src/main/java/com/example/nearbyfoursquare/data/local/dao/PlacesDao.kt
+++ app/src/main/java/com/example/nearbyfoursquare/data/local/dao/PlacesDao.kt
@@ -4,16 +4,16 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.example.nearbyfoursquare.data.local.entity.PlacesEntity
+import com.example.nearbyfoursquare.data.local.entity.PlaceEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PlacesDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAll(repositories: List<PlacesEntity>)
+    suspend fun insertAll(repositories: List<PlaceEntity>)
 
     @Query("SELECT * FROM places")
-    fun getAllPlaces(): Flow<List<PlacesEntity>>
+    fun getAllPlaces(): Flow<List<PlaceEntity>>
 
     @Query("DELETE FROM places")
     suspend fun clearAll()
