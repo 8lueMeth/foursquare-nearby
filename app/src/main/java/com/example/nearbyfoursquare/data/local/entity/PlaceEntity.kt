b5f11b228a88d89347d93816f3bb1506c263a670
Mohsen Farahani <methdfarahani@gmail.com>
2022-01-09 14:29:18 +0330

    Rename PlaceEntity.kt

--- app/src/main/java/com/example/nearbyfoursquare/data/local/entity/PlaceEntity.kt
+++ app/src/main/java/com/example/nearbyfoursquare/data/local/entity/PlaceEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "places")
-data class PlacesEntity(
+data class PlaceEntity(
     @PrimaryKey
     val id: String,
     val name: String,
