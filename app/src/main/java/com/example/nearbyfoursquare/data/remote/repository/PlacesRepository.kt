dabb81dcaf33c7348aba1f14e4c264b365a9e201
Mohsen Farahani <methdfarahani@gmail.com>
2022-01-09 14:29:36 +0330

    Implement PlaceDetails Data Layer

--- app/src/main/java/com/example/nearbyfoursquare/data/remote/repository/PlacesRepository.kt
+++ app/src/main/java/com/example/nearbyfoursquare/data/remote/repository/PlacesRepository.kt
@@ -3,7 +3,8 @@ package com.example.nearbyfoursquare.data.remote.repository
 import androidx.paging.ExperimentalPagingApi
 import androidx.room.withTransaction
 import com.example.nearbyfoursquare.data.local.PlacesDatabase
-import com.example.nearbyfoursquare.data.local.entity.PlacesEntity
+import com.example.nearbyfoursquare.data.local.entity.PlaceDetailsEntity
+import com.example.nearbyfoursquare.data.local.entity.PlaceEntity
 import com.example.nearbyfoursquare.data.remote.service.PlacesApiService
 import com.example.nearbyfoursquare.util.Resource
 import com.example.nearbyfoursquare.util.networkBoundResource
@@ -17,15 +18,16 @@ class PlacesRepository @Inject constructor(
 ) {
 
     private val placesDao = placesDatabase.placesDao()
+    private val placeDetailsDao = placesDatabase.placeDetailsDao()
 
-    fun getPlaces(ll: String): Flow<Resource<List<PlacesEntity>>> = networkBoundResource(
+    fun getPlaces(ll: String): Flow<Resource<List<PlaceEntity>>> = networkBoundResource(
         query = { placesDao.getAllPlaces() },
         fetch = {
             val response = placesApiService.getNearbyPlaces(ll = ll)
 
             val places = response.body()?.results
             places?.map {
-                PlacesEntity(
+                PlaceEntity(
                     id = it.fsq_id,
                     name = it.name,
                     distance = it.distance,
@@ -42,6 +44,31 @@ class PlacesRepository @Inject constructor(
             placesDatabase.withTransaction {
                 placesDao.clearAll()
                 placesDao.insertAll(entities)
+            }
+        },
+        shouldFetch = { true },
+    )
+
+    fun getPlaceDetails(id: String): Flow<Resource<PlaceDetailsEntity>> = networkBoundResource(
+        query = { placeDetailsDao.getPlaceDetails(id) },
+        fetch = {
+            val response = placesApiService.getPlaceDetails(id)
+
+            val details = response.body() ?: throw Exception()
+            PlaceDetailsEntity(
+                fsqId = details.fsq_id,
+                name = details.name,
+                description = details.description,
+                tel = details.tel,
+                address = details.location.address,
+                photo = details.photos?.getOrNull(0)?.getImage()
+                    ?: details.categories?.getOrNull(0)?.getIcon()
+            )
+        },
+        saveFetchResult = { entity ->
+            placesDatabase.withTransaction {
+                placeDetailsDao.deleteById(id)
+                placeDetailsDao.insertPlace(entity)
             }
         },
         shouldFetch = { true },
--- app/src/main/java/com/example/nearbyfoursquare/data/remote/service/PlacesApiService.kt
+++ app/src/main/java/com/example/nearbyfoursquare/data/remote/service/PlacesApiService.kt
@@ -1,8 +1,10 @@
 package com.example.nearbyfoursquare.data.remote.service
 
+import com.example.nearbyfoursquare.data.remote.model.Details
 import com.example.nearbyfoursquare.data.remote.model.ResponseWrapper
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PlacesApiService {
@@ -12,4 +14,9 @@ interface PlacesApiService {
         @Query("limit") limit: Int? = 10,
     ): Response<ResponseWrapper>
 
+    @GET("{fsq_id}")
+    suspend fun getPlaceDetails(
+        @Path("fsq_id") fsqId: String?,
+        @Query("fields") limit: String? = "fsq_id,categories,name,description,tel,rating,location",
+    ): Response<Details>
 }
