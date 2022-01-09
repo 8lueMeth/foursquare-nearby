b58c56086bd6c601480599148b568612d1d15c9a
Mohsen Farahani <methdfarahani@gmail.com>
2022-01-09 14:25:46 +0330

    Add Nav Arguments To Screens.kt

--- app/src/main/java/com/example/nearbyfoursquare/ui/Screens.kt
+++ app/src/main/java/com/example/nearbyfoursquare/ui/Screens.kt
@@ -1,6 +1,13 @@
 package com.example.nearbyfoursquare.ui
 
-sealed class Screens(val route: String) {
-    object Places : Screens(route = "places")
-    object Details : Screens(route = "details")
+import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+
+sealed class Screens(val route: String, val arguments: List<NamedNavArgument>) {
+    object Places : Screens(route = "places", emptyList())
+    object Details : Screens(
+        route = "details",
+        arguments = listOf(navArgument("placeId") { type = NavType.StringType })
+    )
 }
